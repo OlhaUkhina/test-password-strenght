af07232def10f05af5d9e6493aee37249fbd6328
olarismoon <olarismoon@gmail.com>
2023-08-01 12:41:21 +0300

    done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'test-password-strenght';
+  passwordForm: FormGroup;
+  strenght = 0;
+  constructor() {
+    this.passwordForm = new FormGroup({
+      password: new FormControl('', [Validators.required, Validators.minLength(8)])
+    })
+    this.passwordForm.get("password")?.valueChanges.subscribe(x => {
+      this.strenght = 0;
+      if (this.isSomeSpecialCase()) this.strenght++
+
+      if (this.isSomeLetters()) this.strenght++
+
+      if (this.isSomeNumbers()) this.strenght++
+
+    })
+  }
+
+  isSomeSpecialCase() {
+    return /[^A-Za-z0-9]/.test(this.passwordForm.value.password)
+  }
+  isSomeLetters() {
+    return /[A-Za-z]/.test(this.passwordForm.value.password)
+  }
+  isSomeNumbers() {
+    return /[0-9]/.test(this.passwordForm.value.password)
+  }
 }
